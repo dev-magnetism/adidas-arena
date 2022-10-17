508b3b18bbe97d05aef1a96961fbbc799273dc8c
damienmontastier <montastier.damien@gmail.com>
2022-10-17 10:25:00 +0200

    wip

--- components/app/home/Hero.vue
+++ components/app/home/Hero.vue
@@ -91,19 +91,17 @@ export default {
     mm.add('(min-width: 800px)', () => {
       this.tl = gsap.timeline({ repeat: -1, paused: true })
 
-      console.log('testrtttt', this)
-
-      // this.tl.to(this.$refs.union.$el, {
-      //   yPercent: 105,
-      //   duration: 0.675,
-      // })
-      // this.tl.set(this.$refs.union.$el, {
-      //   yPercent: -105,
-      // })
-      // this.tl.to(this.$refs.union.$el, {
-      //   yPercent: 0,
-      //   duration: 0.675,
-      // })
+      this.tl.to(this.$refs.union.$el, {
+        yPercent: 105,
+        duration: 0.675,
+      })
+      this.tl.set(this.$refs.union.$el, {
+        yPercent: -105,
+      })
+      this.tl.to(this.$refs.union.$el, {
+        yPercent: 0,
+        duration: 0.675,
+      })
     })
   },
   beforeDestroy() {
