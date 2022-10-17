314a724dc4d0123aebf376e9e5e689ffaf626f55
damienmontastier <montastier.damien@gmail.com>
2022-10-17 11:31:56 +0200

    wip

--- nuxt.config.js
+++ nuxt.config.js
@@ -192,8 +192,7 @@ export default {
     },
     transpile: ['three', 'gsap'],
     extend(config, ctx) {
-      // config.resolve.alias.vue = 'vue/dist/vue.common'
-      config.resolve.alias.vue = 'vue/dist/vue.esm.js'
+      config.resolve.alias.vue = 'vue/dist/vue.common'
       config.plugins.push(new webpack.ProvidePlugin({ THREE: 'three' }))
       config.module.rules.push({
         test: /\.(glsl|vs|fs)$/,
