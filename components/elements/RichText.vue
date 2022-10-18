2f3dbd654bd62607e083b4bd08cb24120b7135d3
damienmontastier <montastier.damien@gmail.com>
2022-10-18 18:09:39 +0200

    wip animation homepage + projet page

--- components/elements/RichText.vue
+++ components/elements/RichText.vue
@@ -59,13 +59,15 @@ export default {
   },
 
   mounted() {
-    document.fonts.ready.then(() => {
-      if (!this.split) return
+    // document.fonts.ready.then(() => {
+    if (!this.split) return
 
-      console.log('init split')
+    console.log('init split')
 
+    setTimeout(() => {
       this.initSplitText()
-    })
+    }, 500)
+    // })
   },
 
   methods: {
