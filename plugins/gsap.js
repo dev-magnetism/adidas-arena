97fa166d24e363d1cbb91371d5301b5b4b717bda
damienmontastier <montastier.damien@gmail.com>
2022-10-18 06:30:41 +0200

    wip

--- plugins/gsap.js
+++ plugins/gsap.js
@@ -1,25 +1,19 @@
 // import Vue from 'vue'
 
 import { gsap } from 'gsap'
-// import { VelocityTracker } from 'gsap/utils/VelocityTracker'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
-// import { Draggable } from 'gsap/Draggable'
 import { SplitText } from 'gsap/SplitText'
 import { DrawSVGPlugin } from 'gsap/DrawSVGPlugin'
 import { Observer } from 'gsap/Observer'
-// import { InertiaPlugin } from 'gsap/InertiaPlugin'
 
 gsap.config({
   force3D: true,
 })
 
-gsap.registerPlugin(
-  // Draggable,
-  SplitText,
-  Observer,
-  ScrollTrigger,
-  DrawSVGPlugin
-  // InertiaPlugin
-)
+gsap.registerPlugin(Observer, ScrollTrigger, DrawSVGPlugin)
+
+if (process.client) {
+  gsap.registerPlugin(SplitText)
+}
 
 // Vue.prototype.$gsap = gsap
