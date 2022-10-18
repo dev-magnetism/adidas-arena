3911ff776f22cb0bee855a8f5f03d417f888273e
damienmontastier <montastier.damien@gmail.com>
2022-10-18 06:29:48 +0200

    wip

--- components/elements/RichText.vue
+++ components/elements/RichText.vue
@@ -1,3 +1,9 @@
+<template>
+  <div class="app-element-rich-text">
+    <component :is="processedHtml" ref="component" />
+  </div>
+</template>
+
 <script>
 import JSSoup from 'jssoup'
 import { gsap } from 'gsap'
@@ -27,10 +33,62 @@ export default {
       require: true,
     },
   },
-  computed: {},
-  created() {
-    this.soup = new JSSoup(this.content)
+  computed: {
+    processedHtml() {
+      const soup = new JSSoup(this.content)
+
+      const lotties = soup.findAll(undefined, 'lottie-word')
+      const texts = soup.findAll(undefined, 'wysiwyg-text')
+      const strokeTexts = soup.findAll('em')
+      const boldTexts = soup.findAll('strong')
+
+      texts.forEach((text) => {
+        const componentName = text.attrs.class
+          .replace('wysiwyg-text', '')
+          .replace(/ /g, '')
+
+        text.name = `T${componentName}`
+
+        // delete text.attrs.class
+        delete text.attrs.style
+      })
+
+      boldTexts.forEach((text) => {
+        const string = text.getText()
+
+        const stringDecode = decode(string)
+
+        const finalString = stringDecode.replace(/\S+/g, (a, b, c) => {
+          return `<span class="bold">` + a + '</span>'
+        })
+
+        text.replaceWith(finalString)
+
+        delete text.attrs.style
+        delete text.attrs.class
+      })
+
+      strokeTexts.forEach((text) => {
+        text.name = `AtomsTextStroke`
+
+        delete text.attrs.style
+        delete text.attrs.class
+      })
+
+      lotties.forEach((lottie) => {
+        lottie.name = 'ELottieWord'
+        lottie.attrs.class = lottie.attrs.id
+
+        delete lottie.attrs.style
+      })
+
+      return {
+        template: soup.prettify(),
+      }
+    },
   },
+
+  created() {},
   mounted() {
     document.fonts.ready.then(() => {
       if (!this.split) return
@@ -61,6 +119,8 @@ export default {
             linesClass: 'line',
           })
         }
+
+        console.log(this.splitting)
 
         let scrollTrigger
 
@@ -143,57 +203,6 @@ export default {
       }
       return split
     },
-  },
-  render(h) {
-    const lotties = this.soup.findAll(undefined, 'lottie-word')
-    const texts = this.soup.findAll(undefined, 'wysiwyg-text')
-    const strokeTexts = this.soup.findAll('em')
-    const boldTexts = this.soup.findAll('strong')
-
-    texts.forEach((text) => {
-      const componentName = text.attrs.class
-        .replace('wysiwyg-text', '')
-        .replace(/ /g, '')
-
-      text.name = `T${componentName}`
-
-      // delete text.attrs.class
-      delete text.attrs.style
-    })
-
-    boldTexts.forEach((text) => {
-      const string = text.getText()
-
-      const stringDecode = decode(string)
-
-      const finalString = stringDecode.replace(/\S+/g, (a, b, c) => {
-        return `<span class="bold">` + a + '</span>'
-      })
-
-      text.replaceWith(finalString)
-
-      delete text.attrs.style
-      delete text.attrs.class
-    })
-
-    strokeTexts.forEach((text) => {
-      text.name = `AtomsTextStroke`
-
-      delete text.attrs.style
-      delete text.attrs.class
-    })
-
-    lotties.forEach((lottie) => {
-      lottie.name = 'ELottieWord'
-      lottie.attrs.class = lottie.attrs.id
-
-      delete lottie.attrs.style
-    })
-
-    return h({
-      template:
-        '<div class="app-element-rich-text">' + this.soup.prettify() + '</div>',
-    })
   },
 }
 </script>
