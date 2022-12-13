6730d1775feb626e084896484184d23e6eeefa3e
damienmontastier <montastier.damien@gmail.com>
2022-12-13 01:38:30 +0100

    integration exterior inside page

--- components/app/Preloader.vue
+++ components/app/Preloader.vue
@@ -173,7 +173,7 @@ export default {
     onProgressLoader({ normalized }, id) {
       this.tlLoading.to(this, {
         tweenValue: normalized,
-        duration: this.genRand(0.45, 0.85, 2),
+        duration: this.genRand(0.65, 2, 2),
         ease: 'power3.inOut',
       })
 
