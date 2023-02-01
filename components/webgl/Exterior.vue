c794d576f46277e39c9329b241a2bc2e754c736b
damienmontastier <montastier.damien@gmail.com>
2023-02-01 18:07:13 +0100

    fix 3d on all page

--- components/webgl/Exterior.vue
+++ components/webgl/Exterior.vue
@@ -86,7 +86,7 @@ export default {
       this.initExterior()
     },
     modelCloudLoaded() {
-      // this.initClouds()
+      this.initClouds()
     },
     allLoadedActual(payload) {
       if (payload) this.initGUI()
@@ -107,7 +107,7 @@ export default {
 
     if (this.allLoadedActual) {
       this.initExterior()
-      // this.initClouds()
+      this.initClouds()
       this.initGUI()
     }
 
