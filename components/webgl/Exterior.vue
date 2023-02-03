bb7ffcb62ec3b8bb310b59e7d380ac13c4c07dd3
damienmontastier <montastier.damien@gmail.com>
2023-02-03 05:08:06 +0100

    mobile 3d homepage, ui mobile homepage, issues performance on mobile, cleans

--- components/webgl/Exterior.vue
+++ components/webgl/Exterior.vue
@@ -86,7 +86,7 @@ export default {
       this.initExterior()
     },
     modelCloudLoaded() {
-      // this.initClouds()
+      this.initClouds()
     },
     allLoadedActual(payload) {
       if (payload) this.initGUI()
@@ -107,7 +107,7 @@ export default {
 
     if (this.allLoadedActual) {
       this.initExterior()
-      // this.initClouds()
+      this.initClouds()
       this.initGUI()
       this.resetView()
     }
@@ -290,7 +290,7 @@ export default {
       this.initFloor()
       this.initAdidasArena()
       this.initLogoArena()
-      // this.initCars()
+      this.initCars()
       this.initTrams()
       this.initArrow()
 
