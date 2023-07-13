63f407535586cc08c73b4e2ecde42a566acb7f09
Laurent Arnould <l.arnould@magnetism.fr>
2023-07-13 16:50:45 +0200

    Fixes

--- components/app/actus/BlockSlider.vue
+++ components/app/actus/BlockSlider.vue
@@ -112,7 +112,6 @@ export default {
     },
   },
   mounted() {
-    console.log((this.embla)?this.embla:'no carousel')
     this.embla = EmblaCarousel(this.$el, {
       dragFree: true,
       containScroll: 'keepSnaps',
@@ -123,22 +122,17 @@ export default {
       },
     })
 
-    this.embla?.on('pointerUp', this.onPointerUp)
-    this.embla?.on('pointerDown', this.onPointerDown)
-    this.embla?.on('select', this.onSelect)
+    this.embla.on('pointerUp', this.onPointerUp)
+    this.embla.on('pointerDown', this.onPointerDown)
+    this.embla.on('select', this.onSelect)
 
+    //  setTimeout(function(){this.embla?.reInit()} , 500);
   },
   beforeDestroy() {
-  console.log('beforeDestroy')
     this.embla?.off('pointerUp', this.onPointerUp)
     this.embla?.off('pointerDown', this.onPointerDown)
+    this.embla?.off('select', this.onSelect)
 
-    if (!this.$viewport.isMobile) {
-      this.embla?.off('init', this.onScroll)
-      this.embla?.off('scroll', this.onScroll)
-      this.embla?.off('resize', this.onScroll)
-      this.embla?.off('select', this.onSelect)
-    }
     this.embla?.destroy()
   },
   methods: {
@@ -232,6 +226,7 @@ export default {
 <style lang="scss">
 .app-actualites-block-slider {
   position: relative;
+  overflow:hidden;
 
   &__wrapper {
     grid-column: 1 / span 12;
