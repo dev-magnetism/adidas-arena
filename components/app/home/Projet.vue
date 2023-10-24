0d25f66be6aa18f39bb56064ca84dd825ec51faa
Laurent Arnould <l.arnould@magnetism.fr>
2023-10-24 16:58:27 +0200

    Debug index

--- components/app/home/Projet.vue
+++ components/app/home/Projet.vue
@@ -450,7 +450,6 @@ export default {
             '.info-text-3',
             '.info-text-4',
             '.info-text-5',
-            '.info-text-6',
           ],
           {
             y: 10,
