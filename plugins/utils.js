499f292c055261575a98b05c6cfa738469d9dcc0
Laurent Arnould <l.arnould@magnetism.fr>
2023-11-28 18:52:51 +0100

    Fix formatDate on Safari older versions

--- plugins/utils.js
+++ plugins/utils.js
@@ -24,14 +24,14 @@ export default ({ app }, inject) => {
       return _date.replaceAll('-', '/');
     })
 
-    console.log('formatDate / _formatDates', _formatDates);
+    // console.log('formatDate / _formatDates', _formatDates);
 
     const dateObjects = _formatDates
       .map((obj) => new Date(obj))
       .sort((a, b) => a - b)
     const formattedDates = []
     let index = 0
-    console.log('formatDate / dateObjects', dateObjects);
+    // console.log('formatDate / dateObjects', dateObjects);
 
     while (index < dateObjects.length) {
       const currentDate = dateObjects[index]
