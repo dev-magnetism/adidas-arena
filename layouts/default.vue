6600f2c042dde119264b82cc0df0d0535dfc65a5
Laurent Arnould <l.arnould@magnetism.fr>
2024-02-06 11:16:30 +0100

    WIP rebase

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,7 @@
       prod: devToolsHidden,
     }"
   >
-    <!-- SmartAppBannerb / -->
+    <!-- SmartAppBanner / -->
     <div ref="layerBlue" class="app-transition-layer blue" />
     <div ref="layerRed" class="app-transition-layer red" />
     <AppCursor />
