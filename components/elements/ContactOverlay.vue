144e5f72abf658fdbdee3b82c17d58c26391aca6
Laurent Arnould <l.arnould@magnetism.fr>
2024-03-22 12:33:58 +0100

    Changed ContactForm Api

--- components/elements/ContactOverlay.vue
+++ components/elements/ContactOverlay.vue
@@ -156,8 +156,10 @@ export default {
     onSubmit() {
       this.submited = true
 
-      const url =
-        'https://5ovs8mt0r8.execute-api.eu-central-1.amazonaws.com/prod/contact/'
+      //  const url = 'https://5ovs8mt0r8.execute-api.eu-central-1.amazonaws.com/prod/contact/'
+
+
+      const url = 'https://jyoxikyatc.execute-api.us-east-1.amazonaws.com/prod/contact/'
 
       const xhr = new XMLHttpRequest()
 
