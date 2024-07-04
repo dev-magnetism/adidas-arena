e8e422b7ef03f99dc8cf70299dea2c5cced18ccc
Laurent Arnould <l.arnould@magnetism.fr>
2024-07-04 14:32:25 +0200

    fixed specific usecase

--- pages/programmation/_id.vue
+++ pages/programmation/_id.vue
@@ -181,7 +181,7 @@ export default {
   },
 
   mounted() {
-    //  console.log('this.event', this.event);
+    //  console.log('Page event', this.event);
     this.initScrollTrigger()
     this.initMatchMedia()
   },
