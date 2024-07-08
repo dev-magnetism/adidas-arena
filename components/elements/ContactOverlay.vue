77fba3bcefede3a08e7768f9e3081d2b92a5ae8a
Guérot Simon <simon.guerot@gmail.com>
2024-07-08 14:18:26 +0200

    AWS Form Update

--- components/elements/ContactOverlay.vue
+++ components/elements/ContactOverlay.vue
@@ -157,10 +157,10 @@ export default {
       this.submited = true
 
       // on our aws
-      const url = 'https://5ovs8mt0r8.execute-api.eu-central-1.amazonaws.com/prod/contact/'
+      // const url = 'https://5ovs8mt0r8.execute-api.eu-central-1.amazonaws.com/prod/contact/'
 
       // on their aws
-      //  const url = 'https://jyoxikyatc.execute-api.us-east-1.amazonaws.com/prod/contact/'
+      const url = 'https://jyoxikyatc.execute-api.us-east-1.amazonaws.com/prod/contact/'
 
       const xhr = new XMLHttpRequest()
 
