b732b3748c4ff75af2f98494f4615f1a8399a91a
Laurent Arnould <l.arnould@magnetism.fr>
2024-11-20 18:47:27 +0100

    Landing Page Nos Offres

--- components/app/nosoffres/iconServiceATable.vue
+++ components/app/nosoffres/iconServiceATable.vue
@@ -1,10 +1,12 @@
 <template>
-	<svg height="1em" viewBox="0 0 36 36" fill="none">
-		<path d="M11.0774 27.2631L13.5095 25.026L14.7025 20.5153L16.0755 20.4674L16.1531 25.2336L17.5261 25.1856L21.8367 20.6949L23.4646 20.6381L19.9305 26.5545L16.9829 29.3139L11.2332 31.6702" stroke="currentColor" stroke-width="1.1" stroke-miterlimit="10"/>
-		<path d="M7.17269 16.4167C6.96554 10.4848 11.6063 5.50787 17.5385 5.30071C23.4707 5.09355 28.4474 9.73433 28.6545 15.6665L7.17269 16.4167Z" stroke="currentColor" stroke-width="1.1" stroke-miterlimit="10"/>
-		<path d="M17.6861 3.79045L17.2859 3.80442L17.3203 4.78888L17.7205 4.7749L17.6861 3.79045Z" stroke="currentColor" stroke-width="1.1" stroke-miterlimit="10"/>
-		<path d="M32.2094 17.9887L3.81372 18.9803L3.88142 20.9191L32.2771 19.9275L32.2094 17.9887Z" stroke="currentColor" stroke-width="1.1" stroke-miterlimit="10"/>
-		<path d="M7.56592 25.8396L11.0183 25.7191L11.2836 33.3163L7.83122 33.4368" stroke="currentColor" stroke-width="1.1" stroke-miterlimit="10"/>
-		<path d="M9.47615 31.0436L8.40454 31.081L8.44196 32.1526L9.51357 32.1152L9.47615 31.0436Z" fill="currentColor"/>
+	<svg height="1em" viewBox="0 0 1080 1080" fill="none">
+		<polyline stroke-miterlimit="10" stroke-width="33px" stroke="currentColor" points="313.05 817.48 392.74 749.16 435.61 607.29 479.25 607.29 476.43 758.68 520.07 758.68 661.89 620.9 713.63 620.9 594.88 804.8 498.25 889.13 313.11 957.56"/>
+		<g>
+			<path stroke-miterlimit="10" stroke-width="33px" stroke="currentColor" d="M201.11,468.83c0-188.54,152.84-341.39,341.39-341.39s341.39,152.84,341.39,341.39H201.11Z"/>
+			<rect stroke-miterlimit="10" stroke-width="33px" stroke="currentColor" x="536.14" y="79.66" width="12.72" height="31.29"/>
+		</g>
+		<rect stroke-miterlimit="10" stroke-width="33px" stroke="currentColor" x="91.64" y="546.49" width="902.53" height="61.62"/>
+		<polyline stroke-miterlimit="10" stroke-width="33px" stroke="currentColor" points="203.15 768.4 312.88 768.4 312.88 1009.87 203.15 1009.87"/>
+		<rect fill="currentColor" x="223.96" y="935.72" width="34.06" height="34.06"/>
 	</svg>
 </template>
